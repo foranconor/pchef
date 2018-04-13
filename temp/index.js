8770f95dab3fc2dbf0c2e625c23ca56a2c314640
Conor Foran <foranconor@gmail.com>
2018-04-14 11:43:18 +1200

    changes made on raspberry pi device

--- temp/index.js
+++ temp/index.js
@@ -1,6 +1,13 @@
+const fetch = require('node-fetch');
+
+const moment = require('moment');
+
 const SPI = require('pi-spi');
 
 const spi = SPI.initialize('/dev/spidev0.0');
+
+const url = 'https://temperature-logger.herokuapp.com';
+
 // adc must operate between
 spi.clockSpeed(1e6);
 
@@ -8,6 +15,8 @@ spi.clockSpeed(1e6);
 const m = 10.888;
 const c = 0.00347;
 const b = 1777.3;
+
+const DEVICE_ID = 'living room';
 
 let offset = 34;
 
@@ -18,6 +27,7 @@ let t = 0;
 // take n samples at an interval of i milliseconds
 // report the average
 function sample(n, i) {
+  const now = moment().valueOf();
   const a = [];
   const s = setInterval(() => {
     spi.transfer(buf, buf.length, (err, res) => {
@@ -32,14 +42,32 @@ function sample(n, i) {
 
   setTimeout(() => {
     clearInterval(s);
+    a.sort();
+    //const mv = parseFloat(a[a.length/2]);
+    console.log(now);
     const mv = a.reduce((p, q) => p + q, 0) / a.length;
     console.log(mv.toFixed(2) + ' mv');
     t = transfer(mv);
     console.log(t.toFixed(2) + ' \u00B0c');
+    f = t * (9 / 5) + 32;
+    console.log(f.toFixed(2) + ' \u00B0f');
+    fetch(url + '/temp', {
+      method: 'POST',
+      body: JSON.stringify({
+        location: DEVICE_ID,
+        mv: mv,
+        time: now,
+        temp: t
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((v1) => console.log('logging')).catch(err => console.error(err));
+    
   }, i * n);
 }
 
-const u = setInterval(sample, 1000, 50, 30);
+const u = setInterval(sample, 1024, 128, 8);
 
 function transfer(mv) {
   return (m - Math.sqrt((-m)*(-m) + 4 * c * (b - mv))) / (2 * (-c)) + offset;
@@ -49,4 +77,4 @@ function zero() {
   offset = offset - t;
 }
 
-setTimeout(zero, 45000);
+//setTimeout(zero, 45000);
