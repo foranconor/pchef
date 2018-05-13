f414546945da10bf2d041f8b4a3feca90449d8af
forancono <forancono@ecs.vuw.ac.nz>
2018-05-14 11:50:03 +1200

    Drop noisy bit

--- temp/index.js
+++ temp/index.js
@@ -23,7 +23,8 @@ stream.once('open', fd => {
     spi.transfer(a0, a0.length, (err0, res0) => {
       if (err0) console.error(err0);
       else {
-        val = parseInt(res0.toString('hex'), 16) >> 4;
+        val = parseInt(res0.toString('hex'), 16) >> 5;
+        val = val << 1;
         mv = val / 4096 * 3300;
         s0 = transfer(mv);
         spi.transfer(a1, a1.length, (err1, res1) => {
