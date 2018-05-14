5db81fbd989a829e1709fa5011ecdb61d3783281
forancono <forancono@ecs.vuw.ac.nz>
2018-05-14 13:21:30 +1200

    Added logging to terminal

--- temp/index.js
+++ temp/index.js
@@ -13,8 +13,11 @@ let a0 = Buffer.from('c00000', 'hex');
 let a1 = Buffer.from('c80000', 'hex');
 
 let set0 = 37.5;
-let set1 = 34;
+let set1 = 40;
 let i1 = 0;
+
+let kp = 1.0;
+let ki = 0.5;
 
 stream.once('open', fd => {
   setInterval(() => {
@@ -43,7 +46,10 @@ stream.once('open', fd => {
             i1 = i1 + error1;
             // cap i1
             if (i1 < 0) i1 = 0;
-            stream.write(now + ' ' + set1 + ' ' + s1 + ' ' + error1 + ' ' + i1 + ' ' + (error1 + i1) + '\n');
+            if (il > 100) il = 100;
+            let output = now + ' ' + set1 + ' ' + s1 + ' ' + (error1 * kp) + ' ' + (i1 * ki) + ' ' + (kp * error1 + ki * i1) + '\n';
+            stream.write(output);
+            console.log(output);
           }
         });
       }
